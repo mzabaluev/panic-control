52654420276ac2fff1bdb5dbeb495a677951155f
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2017-11-24 23:57:04 +0200

    Redesign for a more ergonomic API
    No more explicit type coercion, yay!
    The JoinHandle'y type got a properly descriptive name,
    ControlledJoinHandle, because nobody cares when they don't have to
    type it.
    
    spawn() is now a method on Context. To call the constructor or the
    From<thread::Builder> impl of Context, it only has to be
    turbofish-qualified with one parameter for the expected panic type.

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,12 @@ pub enum Outcome<T, P> {
 pub type PanicResult<T, P> = thread::Result<Outcome<T, P>>;
 
 #[derive(Debug)]
-pub struct Handle<T, P> {
+pub struct ControlledJoinHandle<T, P> {
     thread_handle: thread::JoinHandle<T>,
     phantom: marker::PhantomData<P>
 }
 
-impl<T, P: Any> Handle<T, P> {
+impl<T, P: Any> ControlledJoinHandle<T, P> {
     pub fn join(self) -> PanicResult<T, P> {
         match self.thread_handle.join() {
             Ok(rv) => Ok(Outcome::NoPanic(rv)),
@@ -47,16 +47,38 @@ impl<T, P: Any> Handle<T, P> {
     pub fn into_thread_join_handle(self) -> thread::JoinHandle<T> { self.thread_handle }
 }
 
-pub fn spawn<T, P, F>(f: F) -> Handle<T, P>
-    where F: FnOnce() -> T,
-          F: Send + 'static,
-          T: Send + 'static,
-          P: Any
-{
-    let thread_handle = thread::spawn(f);
-    Handle {
-        thread_handle: thread_handle,
-        phantom: marker::PhantomData
+pub struct Context<P> {
+    thread_builder: thread::Builder,
+    phantom: marker::PhantomData<P>
+}
+
+impl<P: Any> Context<P> {
+    pub fn new() -> Context<P> {
+        Context {
+            thread_builder: thread::Builder::new(),
+            phantom: marker::PhantomData
+        }
+    }
+
+    pub fn spawn<T, F>(self, f: F) -> ControlledJoinHandle<T, P>
+        where F: FnOnce() -> T,
+              F: Send + 'static,
+              T: Send + 'static
+    {
+        let thread_handle = self.thread_builder.spawn(f).unwrap();
+        ControlledJoinHandle {
+            thread_handle: thread_handle,
+            phantom: self.phantom
+        }
+    }
+}
+
+impl<P: Any> From<thread::Builder> for Context<P> {
+    fn from(builder: thread::Builder) -> Context<P> {
+        Context {
+            thread_builder: builder,
+            phantom: marker::PhantomData
+        }
     }
 }
 
@@ -71,15 +93,15 @@ pub fn chain_hook_ignoring<P: 'static>() {
 
 #[cfg(test)]
 mod tests {
-    use super::spawn;
-    use super::{Handle, Outcome};
+    use super::{Context, Outcome};
     use super::chain_hook_ignoring;
     use std::sync::{Once, ONCE_INIT};
+    use std::thread;
 
     #[derive(Debug, PartialEq, Eq)]
     struct Expected(pub u32);
 
-    fn ignore_expected_panics() {
+    fn silence_expected_panics() {
         static GUARD: Once = ONCE_INIT;
         GUARD.call_once(|| {
             chain_hook_ignoring::<Expected>()
@@ -87,10 +109,26 @@ mod tests {
     }
 
     #[test]
+    fn no_panic() {
+        let ctx = Context::<Expected>::new();
+        let h = ctx.spawn(|| {
+            42
+        });
+        let outcome = h.join().unwrap();
+        assert_eq!(outcome, Outcome::NoPanic(42));
+    }
+
+    #[test]
+    // Have to help the compiler here: without an unreachable return value,
+    // it has troubles inferring the no-panic type parameter of Outcome
+    // and lints on Rust issue #39216.
+    #[allow(unreachable_code)]
     fn expected_panic() {
-        ignore_expected_panics();
-        let h: Handle<(), Expected> = spawn(|| {
+        silence_expected_panics();
+        let ctx = Context::<Expected>::new();
+        let h = ctx.spawn(|| {
             panic!(Expected(42));
+            ()
         });
         let outcome = h.join().unwrap();
         assert_eq!(outcome, Outcome::Panicked(Expected(42)));
@@ -103,7 +141,7 @@ mod tests {
             chain_hook_ignoring::<i32>();
         });
 
-        let h: Handle<(), u32> = spawn(|| {
+        let h = Context::<u32>::new().spawn(|| {
             panic!(42);
         });
         // This wouldn't work:
@@ -111,5 +149,22 @@ mod tests {
         //     assert_eq!(outcome, Outcome::Panicked(42));
         let res = h.join();
         assert!(res.is_err());
+    }
+
+    #[test]
+    fn from_thread_builder() {
+        silence_expected_panics();
+        const THREAD_NAME: &str = "a panicky thread";
+        let thread_builder = thread::Builder::new()
+                                .name(THREAD_NAME.into());
+        let ctx = Context::<Expected>::from(thread_builder);
+        let h = ctx.spawn(|| {
+            let thread = thread::current();
+            let name = thread.name();
+            assert!(name.is_some());
+            assert_eq!(name.unwrap(), THREAD_NAME);
+            42
+        });
+        h.join().unwrap();
     }
 }
